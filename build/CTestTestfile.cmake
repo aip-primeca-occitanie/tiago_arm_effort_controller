c8e35338cf7815686d87896251b440539caf637a
Guillaume <waillyam23@gmail.com>
2022-07-13 13:16:23 +0200

    Cleaning

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pal/pinocchio_ws/src/tiago_arm_effort_controller
-# Build directory: /home/pal/pinocchio_ws/src/tiago_arm_effort_controller/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/devel/share/tiago_arm_effort_controller/cmake/tiago_arm_effort_controller-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(tiago_arm_effort_controller_MESSAGE_FILES "")
-set(tiago_arm_effort_controller_SERVICE_FILES "")
